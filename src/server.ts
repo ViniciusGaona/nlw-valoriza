0f8cb5c3420c56a7353f77a7a77df3acc111b93b
Vinicius Gaona <vinicius.gaona@mzgroup.com>
2021-09-22 23:12:13 -0300

    Aula 2

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,14 @@
+import "reflect-metadata";
 import express from "express";
+
+import { router } from "./routes";
+
+import "./database"
 
 const app = express();
 
-app.get("/test", (request, response) => {
-    return response.send("Olá NLW!");
-});
+app.use(express.json());
 
-app.post("/test-post", (request, response) => {
-    return response.send("Olá NWL método post");
-})
+app.use(router);
 
-app.listen(5000, () => console.log("Server is running"))
+app.listen(5000, () => console.log(`Server is running on port 5000 :)`))
